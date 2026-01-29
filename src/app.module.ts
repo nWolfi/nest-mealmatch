6a6dba7efd36b22fd272f1452b828f747b17a8a9
W_lfle, Noel <noel.woelfle@sybit.de>
2026-01-29 14:19:45 +0100

    added migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IngredientModule } from './modules/ingredient/ingredient.module';
@@ -7,6 +8,17 @@ import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5437,
+      username: 'postgres',
+      password: 'postgres', // Ändere dies zu deinem tatsächlichen Passwort
+      database: 'mealmatch',
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true, // Aktiviert für Entwicklung
+    }),
     UserModule,
     IngredientModule,
     MealModule,
